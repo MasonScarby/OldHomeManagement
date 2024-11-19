deb36c8452ff305426b9e990680759bff3dce4a9
Xavier Shefcyk <147159874+flight04@users.noreply.github.com>
2024-11-19 08:57:34 -0500

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,14 @@
 import './bootstrap';
 
-if (document.body.classList.contains("createAppointments")) {
+if (document.body.classList.contains("createAppointment")) {
     import('../css/createAppointment.css');
 }
 else if (document.body.classList.contains("roles")) {
     import('../css/roles.css');
-
 }
 else if (document.body.classList.contains("employees")) {
     import('../css/employees.css');
 }
-
+else if (document.body.classList.contains("patientAssignment")) {
+    import('../css/patientAssignment.css');
+}
